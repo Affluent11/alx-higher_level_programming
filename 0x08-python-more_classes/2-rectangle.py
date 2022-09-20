d4330fef4e6b089544797e6862d98c45579e1ace
Affluent11 <105013497+Affluent11@users.noreply.github.com>
2022-09-20 02:34:57 +0100

    Add files via upload
    A class rectangle that defines

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -10,13 +10,11 @@ class Rectangle:
 
     @property
     def width(self):
-        """Returns the width of the rectangle"""
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        """Sets the width of the rectangle"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -24,14 +22,22 @@ class Rectangle:
 
     @property
     def height(self):
-        """Returns the height of the rectangle"""
         return (self.__height)
 
     @height.setter
     def height(self, value):
-        """Sets the height of rectangle"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """return the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,1 +1,1 @@
-A class rectangle
+This is a file that contains 0x08-python-more_classes
